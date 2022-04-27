fb7e9beb7cacd87ca53d1012d72e1c8dc4db01f1
Luan Pham <phamquiluan@gmail.com>
2022-04-27 11:58:54 +0700

    Add pre-commit configurations

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="jdeskew",
+    description="Document Image Skew Estimation using Adaptive Radial Projection",
+    author="Luan Pham",
+    author_email="phamquiluan@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["numpy", "opencv-python"],
+    extra_requires={"dev": ["black"]},
+)
