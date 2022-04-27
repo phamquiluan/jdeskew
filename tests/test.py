e09f4dba8647eeb2e03814385d3654a8bb0ddac2
Luan Pham <phamquiluan@gmail.com>
2022-04-27 14:25:33 +0700

    Add basic test case

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,9 @@
-def basic_test():
-    from jdeskew import JDeskew
-    image = 0
-    JDeskew.process(image)
+import numpy as np
+
+
+def test_basic():
+    from jdeskew import get_angle
+
+    image = np.zeros((512, 512), dtype=np.uint8)
+
+    assert get_angle(image) == 0.
