f114d05ce64a14aca4917fdcf6e506ed28886796
Luan Pham <phamquiluan@gmail.com>
2022-04-27 14:54:39 +0700

    Add coverage to setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=["numpy", "opencv-python"],
-    extra_requires={"dev": ["black", "pytest"]},
+    extra_requires={"dev": ["black", "pytest", "coverage"]},
 )
