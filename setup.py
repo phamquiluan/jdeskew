04bd33c984416e1e0caf44c6e344182baaf2c79b
Luan Pham <luan@cinnamon.is>
2022-04-28 11:22:00 +0700

    Add pre-commit as extras requirement

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=["numpy", "opencv-python"],
-    extras_require={"dev": ["black", "pytest", "coverage"]},
+    extras_require={"dev": ["black", "pytest", "coverage", "pre-commit"]},
 )
