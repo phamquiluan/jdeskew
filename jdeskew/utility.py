5cb533e502f0440e8cde60b249188717cc0f8061
Luan Pham <luan@cinnamon.is>
2022-04-28 13:46:50 +0700

    Update default rotate border mode

--- jdeskew/utility.py
+++ jdeskew/utility.py
@@ -12,7 +12,7 @@ def rotate(image, angle, resize=True, border_mode=None, border_value=None):
     border_value : when border_mode == cv2.BORDER_CONSTANT
     """
     if border_mode is None:
-        border_mode = cv2.BORDER_REPLICATE
+        border_mode = cv2.BORDER_CONSTANT
 
     h, w = image.shape[:2]
     M = cv2.getRotationMatrix2D(center=(w // 2, h // 2), angle=angle, scale=1.0)
