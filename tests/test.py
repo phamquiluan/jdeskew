ab80df691dece3bf40173c8bcc5c80a3e5e69e3b
Luan Pham <luan@cinnamon.is>
2022-04-28 11:40:51 +0700

    Add functional test

--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,20 @@
+from os import path
+
+import cv2
 import numpy as np
+
+from jdeskew.estimator import get_angle
+from jdeskew.utility import rotate
 
 
 def test_basic():
-    from jdeskew import get_angle
-
     image = np.zeros((512, 512), dtype=np.uint8)
+    assert get_angle(image) == 0.0
 
-    assert get_angle(image) == 0.
+
+def test_text_image_multiple_angle():
+    image = cv2.imread(path.join(path.dirname(__file__), "test.png"))
+    for angle in range(-10, 10):
+        skew_image = rotate(image, angle=angle, resize=False)
+        estimated_angle = get_angle(skew_image, vertical_image_shape=512)
+        assert abs(angle + estimated_angle) < 0.5, f"{angle} - {estimated_angle}"
