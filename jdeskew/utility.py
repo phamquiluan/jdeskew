45731aedda961fb1cdf80d63cac7fb1c45ddafb8
Luan Pham <luan@cinnamon.is>
2022-04-28 11:38:36 +0700

    Add jdeskew/utility for support functions

---
+++ jdeskew/utility.py
@@ -0,0 +1,31 @@
+import cv2
+
+
+def rotate(image, angle, resize=True, border_mode=None, border_value=None):
+    """
+    rotate input image respect to a given angle
+
+    Params:
+    --------
+    resize (Bool) : resize to input image shape
+    border_mode : cv2.BORDER_REPLICATE, cv2.BORDER_CONSTANT
+    border_value : when border_mode == cv2.BORDER_CONSTANT
+    """
+    if border_mode is None:
+        border_mode = cv2.BORDER_REPLICATE
+
+    h, w = image.shape[:2]
+    M = cv2.getRotationMatrix2D(center=(w // 2, h // 2), angle=angle, scale=1.0)
+    output_image = cv2.warpAffine(
+        src=image,
+        M=M,
+        dsize=(w, h),
+        # flags=cv2.INTER_CUBIC,
+        flags=cv2.INTER_NEAREST,
+        borderMode=border_mode,
+        borderValue=None if border_mode == cv2.BORDER_REPLICATE else border_value,
+    )
+
+    if resize is True:
+        output_image = cv2.resize(output_image, (w, h))
+    return output_image
