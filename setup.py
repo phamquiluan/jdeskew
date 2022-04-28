65153dd529f4d70bf11119b2798f2f69dd2f678b
Luan Pham <luan@cinnamon.is>
2022-04-28 14:30:32 +0700

    Add long description

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 """Setup file for jdeskew package."""
 from setuptools import find_packages, setup
 
+version = "0.0.2"
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="jdeskew",
     description="Document Image Skew Estimation using Adaptive Radial Projection",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Luan Pham",
-    version="0.0.1",
+    version=version,
     author_email="phamquiluan@gmail.com",
     packages=find_packages(),
     include_package_data=True,
